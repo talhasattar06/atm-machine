838813cdacfcca962d63a542601ba6d64d14018e
Talha Sattar <160661345+talhasattar06@users.noreply.github.com>
2024-05-05 07:57:23 +0500

    decorated with chalk

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # atm-machine
-An ATM (Automatic Teller Machine) created with TypeScript, Node.js &amp; Inquirer
+An ATM (Automatic Teller Machine) created with TypeScript, Node.js, Inquirer &amp; Chalk.
--- index.ts
+++ index.ts
@@ -1,55 +1,57 @@
 #! /usr/bin/env node
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 let myBalance: number = 10000;
 const myPin: number = 12345;
+console.log(chalk.hex('FFD400')("---------- Welcome to ATM Machine ----------"));
 
 let pinAnswer = await inquirer.prompt([
   {
     name: "pin",
-    message: "Enter your pin number: ",
+    message: chalk.hex('93E1D8')("Enter your pin number: "),
     type: "number",
   },
 ]);
 
 if (pinAnswer.pin === myPin) {
-  console.log("Correct Pin Code!");
+  console.log(chalk.hex('AFFC41')("Correct Pin Code!"));
 
   let operationAnswer = await inquirer.prompt([
     {
       name: "operation",
-      message: "Select an option:",
+      message: chalk.hex('93E1D8')("Select an option:"),
       type: "list",
-      choices: ["Withdraw", "Check Balance", "Fast Cash"],
+      choices: [chalk.hex('FFA69E')("Withdraw"), chalk.hex('EA638C')("Check Balance"), chalk.hex('89023E')("Fast Cash")]
     },
   ]);
 
-  if (operationAnswer.operation === "Withdraw") {
+  if (operationAnswer.operation === chalk.hex('FFA69E')("Withdraw")) {
     let amountAnswer = await inquirer.prompt([
       {
         name: "amount",
-        message: "Enter your amount: ",
+        message: chalk.hex('D90638')("Enter your amount: "),
         type: "number",
       },
     ]);
 
     if (amountAnswer.amount > myBalance) {
-      console.log("Insufficent balance, can not withdraw your amount.");
+      console.log(chalk.hex('F42B03')("Insufficient balance, can not withdraw your amount."));
 
     } else {
       myBalance -= amountAnswer.amount;
-      console.log(`Your remaining balance is ${myBalance}.`);
+      console.log(chalk.hex('AC46A1')(`Your remaining balance is ${myBalance}.`));
     }
 
-  } else if (operationAnswer.operation === "Check Balance") {
-    console.log(`Your balance is ${myBalance}`);
+  } else if (operationAnswer.operation === chalk.hex('EA638C')("Check Balance")) {
+    console.log(chalk.hex('A663CC')(`Your balance is ${myBalance}`));
 
-  } else if (operationAnswer.operation === "Fast Cash") {
+  } else if (operationAnswer.operation === chalk.hex('89023E')("Fast Cash")) {
     let amountOptions = await inquirer.prompt([
       {
         name: "options",
-        message: "Select an amount: ",
+        message: chalk.hex('01497C')("Select an amount: "),
         type: "list",
         choices: [1000, 5000, 10000, 20000],
       },
@@ -57,16 +59,16 @@ if (pinAnswer.pin === myPin) {
 
     if (amountOptions.options <= myBalance) {
       myBalance -= amountOptions.options;
-      console.log(`Your remaining balance is ${myBalance}.`);
+      console.log(chalk.hex('7B2CBF')(`Your remaining balance is ${myBalance}.`));
 
     } else {
-      console.log("Insufficent balance, can not withdraw your amount.");
+      console.log(chalk.hex('F42B03')("Insufficient balance, can not withdraw your amount."));
     }
 
   } else {
-    console.log("Please select a valid option.");
+    console.log(chalk.hex('BF0603')("Please select a valid option."));
   }
-  
+
 } else {
-  console.log("Incorrect Pin Code!!!");
+  console.log(chalk.hex('D00000')("Incorrect Pin Code!!!"));
 }
